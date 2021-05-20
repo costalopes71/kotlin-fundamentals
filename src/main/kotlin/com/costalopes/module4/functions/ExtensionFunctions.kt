cebaa9f8f6180a95c5ebfdd186e96022d03f5aaf
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-05-19 23:21:22 -0300

    module4: extension functions, infix funcitons and operator overload

---
+++ src/main/kotlin/com/costalopes/module4/functions/ExtensionFunctions.kt
@@ -0,0 +1,22 @@
+package com.costalopes.module4.functions
+
+fun main(args: Array<String>) {
+    val text = "With     multiple \t whitespace"
+
+    // using a helper function
+    println(replaceMultipleSpacesHelper(text))
+
+    // using extension function
+    println(text.replaceMultipleWhitespaces())
+
+}
+
+fun replaceMultipleSpacesHelper(text: String): String {
+    val regex = Regex("\\s+")
+    return regex.replace(text, " ")
+}
+
+fun String.replaceMultipleWhitespaces(): String {
+    val regex = Regex("\\s+")
+    return regex.replace(this, " ")
+}
---
+++ src/main/kotlin/com/costalopes/module4/functions/InfixFunctionsAndOperators.kt
@@ -0,0 +1,26 @@
+package com.costalopes.module4.functions
+
+fun main(args: Array<String>) {
+
+    val h1 = Header("H1")
+    val h2 = Header("H2")
+
+    // using as extension function only
+    val h3 = h1.plus(h2)
+    println(h3.name)
+
+    // using as an Infix function
+    val h4 = h1 plus h2
+    println(h4.name)
+
+    // using as an operator
+    val h5 = h1 + h2
+    println(h5.name)
+
+}
+
+class Header(var name: String) { }
+
+infix operator fun Header.plus(other: Header): Header {
+    return Header("${this.name} ${other.name}")
+}
