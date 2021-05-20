931cf2b149b26edf664ab2149a3a939c85e44723
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-05-20 19:50:26 -0300

    interfaces: default methods, implement multiple interfaces

--- src/main/kotlin/com/costalopes/module5/Interfaces.kt
+++ src/main/kotlin/com/costalopes/module5/Interfaces.kt
@@ -1,4 +1,31 @@
 package com.costalopes.module5
 
-class Interfaces {
+interface Time {
+    fun setTime(hours:Int, mins: Int = 0, secs: Int = 0)
+    // interfaces can have default methods
+    fun setTime(time: KevinTime) = setTime(time.hours)
+}
+
+interface EndOfWorldTime {
+    // default method
+    fun setTime(time: KevinTime) { }
+}
+
+// use : instead of the implements key word
+class YetiTime : Time, EndOfWorldTime {
+    // override key word is mandatory
+    override fun setTime(hours: Int, mins: Int, secs: Int) { }
+    // when implementing multiple interfaces that have default methods with same name, we must override it
+    override fun setTime(time: KevinTime) {
+        // on the method implemantation i can call the interfaces implemantion, one or both
+        super<Time>.setTime(time)
+        super<EndOfWorldTime>.setTime(time)
+    }
+
+}
+
+class KevinTime {
+    var hours: Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
 }
